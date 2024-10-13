473dfa71bd5c3ca456fbc3ac0979bcd8ed16e554
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2024-10-13 20:33:41 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,29 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { initializeApp } from "firebase/app";
+import { importProvidersFrom } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuPuuRyLiGEHJSHcxncqrzSfcz-n95MtE",
+  authDomain: "contact-esiea-e4464.firebaseapp.com",
+  projectId: "contact-esiea-e4464",
+  databaseURL: "https://contact-esiea-e4464-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "contact-esiea-e4464.appspot.com",
+  messagingSenderId: "200549312935",
+  appId: "1:200549312935:web:8dddcc437be16819ee2e82"
+};
+
+initializeApp(firebaseConfig);
+
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    {
+      provide: RouteReuseStrategy, useClass: IonicRouteStrategy
+    },
+      importProvidersFrom(IonicModule.forRoot({innerHTMLTemplatesEnabled: true})),
+      provideIonicAngular(),
+      provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
